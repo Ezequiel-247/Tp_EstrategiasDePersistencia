22e210278bb5c7fe3d27a134d5ec8bec49bf3dfa
jonajofre <jonajofre89@hotmail.com>
2025-05-18 12:50:19 -0300

    schema usuario

--- src/middleware/schemas/usuarioSchema.js
+++ src/middleware/schemas/usuarioSchema.js
@@ -2,15 +2,8 @@ const Joi = require('joi')
 
 const usuarioSchema = Joi.object({
     nombre: Joi.string().min(2).max(50).required(),
-<<<<<<< HEAD
-    //seguir completando
-})
-
-module.exports = usuarioSchema
-=======
     email: Joi.string().email().min(2).max(50).required(),
     contraseña: Joi.string().min(6).max(20).required()
     //Completado falta chequear
 });
 module.exports = usuarioSchema;
->>>>>>> 3e45ad4d602f661ce44e294697d2920544b7a744
