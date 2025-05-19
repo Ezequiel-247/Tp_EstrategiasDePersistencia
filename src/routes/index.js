213608e1d33421f3ca3a081d43eade9b9629bbbc
jonajofre <jonajofre89@hotmail.com>
2025-05-19 16:17:54 -0300

    2

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-const {Router} = require('express');
-const router = Router();
-
-//este archivo es para un router general, que a la vez contiene a los demas.
-
-
-// Agrega los routers aca 
-// por ejemplo -> const publicacionRoutes = require('./publicacionesRoutes');
-const usuarioRouter = require('./usuarioRouter');
-
-//agregar el uso de los routers
-//por ejemplo -> router.use('/publicacion', publicacionRoutes);
-router.use('/usuario', usuarioRouter);
-
-module.exports = router;
-=======
 const express = require("express");
 const app = express();
 
@@ -33,4 +16,3 @@ app.use('/imagenes', imagenesRouter);
 app.use('/etiqueta', etiquetaRouter);
 
 module.exports = app
->>>>>>> 6ab27720a598922873af2408ad93f3ef4fd4a214
